6da8108a21cde141d73ff431eeb571c9eef31e60
saif <saifeddine.chihani@gmail.com>
2021-11-21 10:52:23 +0100

    your commit message

---
+++ HelloWord/src/main/java/IA31/HelloWord/HelloWordApplication.java
@@ -0,0 +1,16 @@
+package IA31.HelloWord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
+
+
+@SpringBootApplication
+public class HelloWordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWordApplication.class, args);
+	}
+
+
+}
---
+++ HelloWord/src/test/java/IA31/HelloWord/HelloWordApplicationTests.java
@@ -0,0 +1,13 @@
+package IA31.HelloWord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
